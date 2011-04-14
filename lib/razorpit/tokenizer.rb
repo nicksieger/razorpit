04f53b04af3958c7881e76c14ba46fb7d8db79ef
MenTaLguY <mental@rydia.net>
2011-04-14 15:11:12 -0700

    tokenize plus sign too

--- lib/razorpit/tokenizer.rb
+++ lib/razorpit/tokenizer.rb
@@ -3,8 +3,34 @@ module RazorPit
 module Tokenizer
   extend self
 
+  BaseTokens = Struct.new :value
+
+  module Tokens
+    NUMBER = Class.new BaseTokens
+    PLUS = Class.new BaseTokens
+  end
+
+  TOKENS_REGEXP = %r{
+    (?<number>(?<value>\d+)) |
+    (?<plus>\+)
+  }x
+
   def tokenize(string)
-    []
+    tokens = []
+
+    m = TOKENS_REGEXP.match(string)
+    if m
+      value = m['value']
+      case
+      when m['number']
+        token = Tokens::NUMBER[value.to_f]
+      when m['plus']
+        token = Tokens::PLUS[]
+      end
+      tokens << token
+    end
+
+    tokens
   end
 end
 
--- spec/tokenizer_spec.rb
+++ spec/tokenizer_spec.rb
@@ -3,7 +3,17 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'razorpit/tokenizer'
 
 describe RazorPit::Tokenizer do
+  Tokens = RazorPit::Tokenizer::Tokens
+
   it "tokenizes an empty string" do
     RazorPit::Tokenizer.tokenize("").should == []
+  end
+
+  it "tokenizes a number" do
+    RazorPit::Tokenizer.tokenize("3").should == [Tokens::NUMBER[3]]
+  end
+
+  it "tokenizes a plus sign" do
+    RazorPit::Tokenizer.tokenize("+").should == [Tokens::PLUS[]]
   end
 end

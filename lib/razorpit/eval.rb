dd23987626f4be8345055b2ef15828d525dcbe24
MenTaLguY <mental@rydia.net>
2011-04-20 14:35:07 -0700

    clean up eval namespace

--- lib/razorpit/eval.rb
+++ lib/razorpit/eval.rb
@@ -1,63 +1,61 @@
 require 'razorpit/nodes'
 
 module RazorPit
-
 module Eval
-  module Semantics
-    LiteralNode.class_eval do
-      def evaluate
-        value
-      end
-    end
 
-    Nodes::UnaryPlus.class_eval do
-      def evaluate
-        expr.evaluate
-      end
-    end
-
-    Nodes::UnaryMinus.class_eval do
-      def evaluate
-        -expr.evaluate
-      end
-    end
-
-    Nodes::Add.class_eval do
-      def evaluate
-        lhs.evaluate + rhs.evaluate
-      end
-    end
-
-    Nodes::Subtract.class_eval do
-      def evaluate
-        lhs.evaluate - rhs.evaluate
-      end
-    end
-
-    Nodes::Multiply.class_eval do
-      def evaluate
-        lhs.evaluate * rhs.evaluate
-      end
-    end
-
-    Nodes::Divide.class_eval do
-      def evaluate
-        lhs.evaluate / rhs.evaluate
-      end
-    end
-
-    Nodes::Modulus.class_eval do
-      def evaluate
-        lhs.evaluate % rhs.evaluate
-      end
-    end
-
-    class << Nodes::NULL
-      def evaluate
-        nil
-      end
-    end
+LiteralNode.class_eval do
+  def evaluate
+    value
   end
 end
 
+Nodes::UnaryPlus.class_eval do
+  def evaluate
+    expr.evaluate
+  end
+end
+
+Nodes::UnaryMinus.class_eval do
+  def evaluate
+    -expr.evaluate
+  end
+end
+
+Nodes::Add.class_eval do
+  def evaluate
+    lhs.evaluate + rhs.evaluate
+  end
+end
+
+Nodes::Subtract.class_eval do
+  def evaluate
+    lhs.evaluate - rhs.evaluate
+  end
+end
+
+Nodes::Multiply.class_eval do
+  def evaluate
+    lhs.evaluate * rhs.evaluate
+  end
+end
+
+Nodes::Divide.class_eval do
+  def evaluate
+    lhs.evaluate / rhs.evaluate
+  end
+end
+
+Nodes::Modulus.class_eval do
+  def evaluate
+    lhs.evaluate % rhs.evaluate
+  end
+end
+
+class << Nodes::NULL
+  def evaluate
+    nil
+  end
+end
+
+end
 end

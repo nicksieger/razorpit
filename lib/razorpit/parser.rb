ca997f7d021984ce55f9ed92d5e2b8375d6776af
MenTaLguY <mental@rydia.net>
2011-04-19 20:57:46 -0700

    parse numeric expressions

--- lib/razorpit/parser.rb
+++ lib/razorpit/parser.rb
@@ -1,22 +1,52 @@
+require 'razorpit/tokenizer'
+
 module RazorPit
-  module Parser
-    extend self
 
-    def parse(string)
-      RazorPit::Nodes::Program[]
+module Parser
+  extend self
+
+  def parse(string)
+    RazorPit::Nodes::Program[]
+  end
+
+  def parse_expression(string)
+    Tokenizer.tokenize(string) do |token|
+      case token
+      when Tokenizer::Tokens::NUMBER
+        return Nodes::Number[token.value]
+      end
+    end
+  end
+end
+
+class Node
+  class << self
+    alias_method :[], :new
+  end
+
+  def ==(other)
+    false
+  end
+end
+
+module Nodes
+  class Number < Node
+    attr_reader :value
+
+    def initialize(value)
+      @value = value
+    end
+
+    def ==(other)
+      self.class == other.class && self.value == other.value
     end
   end
 
-  module Nodes
-    class Program
-      class << self
-        alias_method :[], :new
-      end
-
-      def ==(other)
-        true
-      end
+  class Program < Node
+    def ==(other)
+      self.class == other.class
     end
-
   end
+end
+
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,9 +2,38 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 require 'razorpit/parser'
 
+Module.new do
+
+N = RazorPit::Nodes
+
 describe RazorPit::Parser do
-  it "parses an empty string" do
+  it "parses an empty program" do
     ast = RazorPit::Parser.parse("")
-    ast.should == RazorPit::Nodes::Program[]
+    ast.should == N::Program[]
   end
+
+  it "parses an expression" do
+    ast = RazorPit::Parser.parse_expression("1")
+    ast.should == N::Number[1]
+  end
+end
+
+describe "#{RazorPit::Node}#==" do
+  it "considers two equivalent program instances to be equal" do
+    N::Program[].should == N::Program[]
+  end
+
+  it "considers two number instances with equal values to be equal" do
+    N::Number[1].should == N::Number[1]
+  end
+
+  it "considers two number instances with different values to be not equal" do
+    N::Number[1].should_not == N::Number[2]
+  end
+
+  it "has a == method which distinguishes node classes" do
+    N::Program[].should_not == N::Number[1]
+  end
+end
+
 end

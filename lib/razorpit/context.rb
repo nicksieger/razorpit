c0a3ca743cbdb8ac80e2a9e2ee5318fee9dd3abf
MenTaLguY <mental@rydia.net>
2011-04-24 12:30:27 -0700

    follow therubyracer behavior in converting both null and undefined to Ruby nil

--- lib/razorpit/context.rb
+++ lib/razorpit/context.rb
@@ -4,9 +4,21 @@ require 'razorpit/eval'
 module RazorPit
 
 class Context
+  module Helpers
+    extend self
+    def to_ruby(value)
+      case value
+      when RazorPit::NULL
+        nil
+      else
+        value
+      end
+    end
+  end
+
   def eval(string)
     ast = Parser.parse_expression(string)
-    ast.evaluate
+    Helpers.to_ruby(ast.evaluate)
   end
 end
 
--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -19,6 +19,33 @@ describe "#{RazorPit::Context}#eval" do
   it "evaluates a trivial expression" do
     @ctx.eval("1").should == 1.0
   end
+
+  it "returns numbers as floats" do
+    result = @ctx.eval("1")
+    result.should be_an_instance_of(Float)
+  end
+
+  it "returns strings as strings" do
+    result = @ctx.eval("'foo'")
+    result.should be_an_instance_of(String)
+  end
+
+  it "returns booleans as booleans" do
+    result = @ctx.eval("true")
+    result.should be_true
+    result = @ctx.eval("false")
+    result.should be_false
+  end
+
+  it "returns undefined as nil" do
+    result = @ctx.eval("void 0")
+    result.should be_nil
+  end
+
+  it "returns null as nil" do
+    result = @ctx.eval("null")
+    result.should be_nil
+  end
 end
 
 end

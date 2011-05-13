59b93088fb32294fd4542c9a6c1ad44320ba95ff
MenTaLguY <mental@rydia.net>
2011-05-12 23:14:05 -0700

    transition further towards parser object

--- lib/razorpit/parser.rb
+++ lib/razorpit/parser.rb
@@ -41,7 +41,7 @@ class Parser
       token_type.token_class_eval do
         include PrefixToken
         eval <<-EOS
-          def prefix(tokens)
+          def prefix(parser)
             #{ast_class}[value]
           end
         EOS
@@ -54,8 +54,8 @@ class Parser
       token_type.token_class_eval do
         include SuffixToken
         eval <<-EOS
-          def suffix(tokens, lhs)
-            rhs = Grammar.expression(tokens, #{binding_power})
+          def suffix(parser, lhs)
+            rhs = parser.expression(#{binding_power})
             #{ast_class}[lhs, rhs]
           end
         EOS
@@ -66,8 +66,8 @@ class Parser
       token_type.token_class_eval do
         include PrefixToken
         eval <<-EOS
-          def prefix(tokens)
-            expr = Grammar.expression(tokens, #{binding_power})
+          def prefix(parser)
+            expr = parser.expression(#{binding_power})
             #{ast_class}[expr]
           end
         EOS
@@ -77,8 +77,8 @@ class Parser
     Tokens::PERIOD.left_binding_power = MEMBER_BINDING_POWER
     Tokens::PERIOD.token_class_eval do
       include SuffixToken
-      def suffix(tokens, lhs)
-        name = Grammar.consume_token(tokens, Tokens::IDENTIFIER)
+      def suffix(parser, lhs)
+        name = parser.consume_token(Tokens::IDENTIFIER)
         Nodes::PropertyAccess[lhs, Nodes::String[name.value]]
       end
     end
@@ -86,9 +86,9 @@ class Parser
     Tokens::OPEN_BRACKET.left_binding_power = MEMBER_BINDING_POWER
     Tokens::OPEN_BRACKET.token_class_eval do
       include SuffixToken
-      def suffix(tokens, lhs)
-        rhs = Grammar.expression(tokens, MEMBER_BINDING_POWER)
-        Grammar.consume_token(tokens, Tokens::CLOSE_BRACKET)
+      def suffix(parser, lhs)
+        rhs = parser.expression(MEMBER_BINDING_POWER)
+        parser.consume_token(Tokens::CLOSE_BRACKET)
         Nodes::PropertyAccess[lhs, rhs]
       end
     end
@@ -98,12 +98,12 @@ class Parser
       include PrefixToken 
       include SuffixToken
 
-      def prefix(tokens)
-        expr = Grammar.expression(tokens, INCREMENT_BINDING_POWER)
+      def prefix(parser)
+        expr = parser.expression(INCREMENT_BINDING_POWER)
         Nodes::PreIncrement[expr]
       end
 
-      def suffix(tokens, lhs)
+      def suffix(parser, lhs)
         Nodes::PostIncrement[lhs]
       end
     end
@@ -113,12 +113,12 @@ class Parser
       include PrefixToken 
       include SuffixToken
 
-      def prefix(tokens)
-        expr = Grammar.expression(tokens, INCREMENT_BINDING_POWER)
+      def prefix(parser)
+        expr = parser.expression(INCREMENT_BINDING_POWER)
         Nodes::PreDecrement[expr]
       end
 
-      def suffix(tokens, lhs)
+      def suffix(parser, lhs)
         Nodes::PostDecrement[lhs]
       end
     end
@@ -128,19 +128,19 @@ class Parser
       include PrefixToken 
       include SuffixToken
 
-      def prefix(tokens)
-        expr = Grammar.expression(tokens, MIN_BINDING_POWER)
-        Grammar.consume_token(tokens, Tokens::CLOSE_PAREN)
+      def prefix(parser)
+        expr = parser.expression(MIN_BINDING_POWER)
+        parser.consume_token(Tokens::CLOSE_PAREN)
         expr
       end
 
-      def suffix(tokens, lhs)
+      def suffix(parser, lhs)
         args = []
-        unless Grammar.try_consume_token(tokens, Tokens::CLOSE_PAREN)
+        unless parser.try_consume_token(Tokens::CLOSE_PAREN)
           begin
-            args << Grammar.expression(tokens, COMMA_BINDING_POWER)
-          end while Grammar.try_consume_token(tokens, Tokens::COMMA)
-          Grammar.consume_token(tokens, Tokens::CLOSE_PAREN)
+            args << parser.expression(COMMA_BINDING_POWER)
+          end while parser.try_consume_token(Tokens::COMMA)
+          parser.consume_token(Tokens::CLOSE_PAREN)
         end
         N::FunctionCall[lhs, *args]
       end
@@ -149,10 +149,10 @@ class Parser
     Tokens::QUESTION.left_binding_power = CONDITION_BINDING_POWER
     Tokens::QUESTION.token_class_eval do
       include SuffixToken
-      def suffix(tokens, lhs)
-        this_expr = Grammar.expression(tokens, COMMA_BINDING_POWER)
-        Grammar.consume_token(tokens, Tokens::COLON)
-        else_expr = Grammar.expression(tokens, COMMA_BINDING_POWER)
+      def suffix(parser, lhs)
+        this_expr = parser.expression(COMMA_BINDING_POWER)
+        parser.consume_token(Tokens::COLON)
+        else_expr = parser.expression(COMMA_BINDING_POWER)
         Nodes::Condition[lhs, this_expr, else_expr]
       end
     end
@@ -160,7 +160,7 @@ class Parser
     Tokens::NULL.left_binding_power = MAX_BINDING_POWER
     Tokens::NULL.token_class_eval do
       include PrefixToken
-      def prefix(tokens)
+      def prefix(parser)
         Nodes::NULL
       end
     end
@@ -233,125 +233,130 @@ class Parser
       token.left_binding_power || MIN_BINDING_POWER
     end
 
-    def expression(tokens, right_binding_power)
-      token = consume_token(tokens, PrefixToken)
-      ast = token.prefix(tokens)
+    def expression(right_binding_power)
+      token = consume_token(PrefixToken)
+      ast = token.prefix(self)
       loop do
-        token = try_consume_token(tokens, Proc.new { |t|
+        token = try_consume_token(Proc.new { |t|
           SuffixToken === t and
           left_binding_power(t) > right_binding_power
         })
         break unless token
-        ast = token.suffix(tokens, ast)
+        ast = token.suffix(self, ast)
       end
       ast
     end
 
-    def empty_statement(tokens)
-      return nil unless try_consume_token(tokens, Tokens::SEMICOLON)
+    def empty_statement
+      return nil unless try_consume_token(Tokens::SEMICOLON)
       Nodes::EmptyStatement[]
     end
 
-    def expression_statement(tokens)
-      ast = expression(tokens, MIN_BINDING_POWER)
-      consume_token(tokens, Tokens::SEMICOLON)
+    def expression_statement
+      ast = expression(MIN_BINDING_POWER)
+      consume_token(Tokens::SEMICOLON)
       ast
     end
 
-    def block_statement(tokens)
-      return nil unless try_consume_token(tokens, Tokens::OPEN_BRACE)
-      statement_list(tokens, Tokens::CLOSE_BRACE) { |s| Nodes::Block[*s] }
+    def block_statement
+      return nil unless try_consume_token(Tokens::OPEN_BRACE)
+      statement_list(Tokens::CLOSE_BRACE) { |s| Nodes::Block[*s] }
     end
 
-    def variable_statement(tokens)
-      return nil unless try_consume_token(tokens, Tokens::VAR)
+    def variable_statement
+      return nil unless try_consume_token(Tokens::VAR)
 
       decls = {}
       begin
-        name = consume_token(tokens, Tokens::IDENTIFIER).value
-        init = if try_consume_token(tokens, Tokens::ASSIGN)
-                 expression(tokens, COMMA_BINDING_POWER)
+        name = consume_token(Tokens::IDENTIFIER).value
+        init = if try_consume_token(Tokens::ASSIGN)
+                 expression(COMMA_BINDING_POWER)
                else
                  nil
                end
         decls[name] = init
-      end while try_consume_token(tokens, Tokens::COMMA)
+      end while try_consume_token(Tokens::COMMA)
 
-      consume_token(tokens, Tokens::SEMICOLON)
+      consume_token(Tokens::SEMICOLON)
       Nodes::VariableStatement[decls]
     end
 
-    def function_declaration(tokens)
-      return nil unless try_consume_token(tokens, Tokens::FUNCTION)
-      name = consume_token(tokens, Tokens::IDENTIFIER).value
+    def function_declaration
+      return nil unless try_consume_token(Tokens::FUNCTION)
+      name = consume_token(Tokens::IDENTIFIER).value
       args = []
-      consume_token(tokens, Tokens::OPEN_PAREN)
-      unless try_consume_token(tokens, Tokens::CLOSE_PAREN)
+      consume_token(Tokens::OPEN_PAREN)
+      unless try_consume_token(Tokens::CLOSE_PAREN)
         begin
-          args << consume_token(tokens, Tokens::IDENTIFIER).value
-        end while try_consume_token(tokens, Tokens::COMMA)
-        consume_token(tokens, Tokens::CLOSE_PAREN)
+          args << consume_token(Tokens::IDENTIFIER).value
+        end while try_consume_token(Tokens::COMMA)
+        consume_token(Tokens::CLOSE_PAREN)
       end
-      consume_token(tokens, Tokens::OPEN_BRACE)
-      statement_list(tokens, Tokens::CLOSE_BRACE) { |s|
+      consume_token(Tokens::OPEN_BRACE)
+      statement_list(Tokens::CLOSE_BRACE) { |s|
         Nodes::FunctionDeclaration[name, args, *s]
       }
     end
 
-    def statement(tokens)
-      empty_statement(tokens) || block_statement(tokens) ||
-      variable_statement(tokens) || function_declaration(tokens) ||
-      expression_statement(tokens)
+    def statement
+      empty_statement || block_statement ||
+      variable_statement || function_declaration ||
+      expression_statement
     end
 
-    def statement_list(tokens, terminator)
+    def statement_list(terminator)
       statements = []
-      until try_consume_token(tokens, terminator)
-        ast = statement(tokens)
+      until try_consume_token(terminator)
+        ast = statement
         statements << ast unless Nodes::EmptyStatement === ast
       end
       yield statements
     end
 
-    def program(tokens)
-      statement_list(tokens, Tokens::EOF) { |s| Nodes::Program[*s] }
+    def program
+      statement_list(Tokens::EOF) { |s| Nodes::Program[*s] }
     end
 
-    def consume_token(tokens, kind)
-      tokens.next while Tokens::LINE_BREAK === tokens.peek
-      token = tokens.peek
-      unless kind === token
-        if kind === Tokens::SEMICOLON
-          case token
-          when Tokens::EOF, Tokens::CLOSE_BRACE
-            return Tokens::SEMICOLON
-          end
-        end
-        raise ParseError, "Expected #{kind} but got #{token}"
-      end
-      tokens.next
-    end
-
-    def try_consume_token(tokens, kind)
-      tokens.next while Tokens::LINE_BREAK === tokens.peek
-      if kind === tokens.peek
-        tokens.next
-      else
-        nil
-      end
-    end
   end
 
   include Grammar
 
+  def initialize(tokens)
+    @tokens = tokens
+  end
+
+  def consume_token(kind)
+    @tokens.next while Tokens::LINE_BREAK === @tokens.peek
+    token = @tokens.peek
+    unless kind === token
+      if kind === Tokens::SEMICOLON
+        case token
+        when Tokens::EOF, Tokens::CLOSE_BRACE
+          return Tokens::SEMICOLON
+        end
+      end
+      raise ParseError, "Expected #{kind} but got #{token}"
+    end
+    @tokens.next
+  end
+
+  def try_consume_token(kind)
+    @tokens.next while Tokens::LINE_BREAK === @tokens.peek
+    if kind === @tokens.peek
+      @tokens.next
+    else
+      nil
+    end
+  end
+
   def self.parse(string)
     tokens = Lexer.scan(string)
-    new.program(tokens)
+    new(tokens).program
   end
 
   def self.parse_expression(string)
     tokens = Lexer.scan(string)
-    new.expression(tokens, Grammar::MIN_BINDING_POWER)
+    new(tokens).expression(Grammar::MIN_BINDING_POWER)
   end
 end
 
